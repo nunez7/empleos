2e501aee0abbe1191df30dc0fb746acf59ce2005
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2021-12-29 16:13:07 -0700

    Creando aplicacion base

---
+++ src/main/java/edu/mx/utdelacosta/EmpleosApplication.java
@@ -0,0 +1,13 @@
+package edu.mx.utdelacosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mx/utdelacosta/EmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mx.utdelacosta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
