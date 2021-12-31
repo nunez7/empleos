368f330fc60ad8a0a5b2bdde57b26d99867af9e0
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2021-12-31 11:22:51 -0700

    Edicion y eliminacion en categorias

--- src/main/java/edu/mx/utdelacosta/controller/CategoriasController.java
+++ src/main/java/edu/mx/utdelacosta/controller/CategoriasController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.mx.utdelacosta.model.Categoria;
+import edu.mx.utdelacosta.model.Vacante;
 import edu.mx.utdelacosta.service.ICategoriasService;
 
 @Controller
@@ -27,7 +30,7 @@ public class CategoriasController {
 
 	// @GetMapping("/create")
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public String crear() {
+	public String crear(Categoria categoria) {
 		return "categorias/formCategoria";
 	}
 
@@ -40,6 +43,20 @@ public class CategoriasController {
 		serviceCategorias.guardar(categoria);
 		attributes.addFlashAttribute("msg", "Registro Guardado");
 		return "redirect:/categorias/index"; 
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String eliminar(@PathVariable("id") int idCategoria, RedirectAttributes attributes) {
+		serviceCategorias.eliminar(idCategoria);
+		attributes.addFlashAttribute("msg", "La categoria fue eliminada");
+		return "redirect:/categorias/index";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String editar(@PathVariable("id") int idCategoria, Model model) {
+		Categoria categoria = serviceCategorias.buscarPorId(idCategoria);
+		model.addAttribute("categoria", categoria);
+		return "categorias/formCategoria";
 	}
 
 }
