e27a3460495b4a4cdb728831447202d196d4b743
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2021-12-31 01:50:06 -0700

    metodo buscarDestacadas actualizado

--- src/main/java/edu/mx/utdelacosta/controller/HomeController.java
+++ src/main/java/edu/mx/utdelacosta/controller/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 import edu.mx.utdelacosta.model.Vacante;
 import edu.mx.utdelacosta.service.IVacantesService;
 
@@ -50,8 +52,16 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
-		model.addAttribute("vacantes", serviceVacantes.buscarDestacadas());
 		return "home";
 	}
 	
+	//Estos atributos estaran disponibles a todos los metodos
+	@ModelAttribute
+	public void setGenericos(Model model) {
+		/*Vacante vacanteSearch = new Vacante();
+		vacanteSearch.reset();
+		model.addAttribute("categorias", serviceCategorias.buscarTodas());
+		model.addAttribute("search",vacanteSearch);*/
+		model.addAttribute("vacantes", serviceVacantes.buscarDestacadas());
+	}
 }
