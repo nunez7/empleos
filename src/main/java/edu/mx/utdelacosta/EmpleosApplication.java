93235e388098edd15bad54cd1f790f2a54e196b6
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2021-12-30 22:48:47 -0700

    Interfaz commandlinerunner

--- src/main/java/edu/mx/utdelacosta/EmpleosApplication.java
+++ src/main/java/edu/mx/utdelacosta/EmpleosApplication.java
@@ -1,13 +1,23 @@
 package edu.mx.utdelacosta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmpleosApplication {
+public class EmpleosApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Ejemplos de Spring Data JPA");
+		guardar();
+	}
+
+	private void guardar() {
+		System.out.println("Insertando un registro");
+	}
 }
