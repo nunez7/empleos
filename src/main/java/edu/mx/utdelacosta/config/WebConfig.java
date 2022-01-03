db4193d55a1eb7332846c7d9a63cd099e83ec10f
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-01-03 12:23:42 -0700

    Agregando nueva ruta de imagenes

--- src/main/java/edu/mx/utdelacosta/config/WebConfig.java
+++ src/main/java/edu/mx/utdelacosta/config/WebConfig.java
@@ -10,10 +10,19 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Value("${empleosapp.ruta.imagenes}")
 	private String rutaImagenes;
+	
+	@Value("${empleosapp.ruta.cv}")
+	private String rutaCv;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //registry.addResourceHandler("/logos/**").addResourceLocations("file:/empleos/img-vacantes/"); // Linux
 		registry.addResourceHandler("/logos/**").addResourceLocations("file:"+rutaImagenes); // Windows
+		
+
+		// Configuración de los recursos estáticos (archivos de los CV)
+		//registry.addResourceHandler("/cv/**").addResourceLocations("file:c:/empleos/files-cv/"); // Windows
+		//registry.addResourceHandler("/cv/**").addResourceLocations("file:/empleos/files-cv/");
+		registry.addResourceHandler("/cv/**").addResourceLocations("file:"+rutaCv); // Linux
 	}
 }
